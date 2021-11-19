ced30156e10874ef289db62aebd06e8da215230a
kasia.kordys <kasia.kordys@gmail.com>
2021-11-19 18:02:20 +0200

    add logic to component with structural directives

--- APM/src/app/products/product-list.component.ts
+++ APM/src/app/products/product-list.component.ts
@@ -7,4 +7,26 @@ import { Component } from "@angular/core";
 
 export class PorductListComponent {
     pageTitle: string = 'Product List';
+    products: any[] = [
+        {
+            "productId": 2,
+            "productName": "Garden Cart",
+            "productCode": "GDN-0023",
+            "releaseDate": "March 18, 2021",
+            "description": "15 gallon capacity rolling garden cart",
+            "price": 32.99,
+            "starRating": 4.2,
+            "imageUrl": "assets/images/garden_cart.png"
+        },
+        {
+            "productId": 8,
+            "productName": "Saw",
+            "productCode": "TBX-0022",
+            "releaseDate": "May 15, 2021",
+            "description": "15-inch steel blade hand saw",
+            "price": 11.55,
+            "starRating": 3.7,
+            "imageUrl": "assets/images/saw.png"
+        }
+    ]
 }
