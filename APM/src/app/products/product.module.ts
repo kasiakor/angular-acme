f65e30149e09ad6ab2ac37218c8d416705fb04b6
kasia.kordys <kasia.kordys@gmail.com>
2021-12-02 17:22:26 +0200

    move feature related items from app module to product module

--- APM/src/app/products/product.module.ts
+++ APM/src/app/products/product.module.ts
@@ -1,12 +1,33 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ProductListComponent } from './product-list.component';
+import { ProductDetailsComponent } from './product-details.component';
+import { ConvertToSpacesPipe } from '../shared/convert-to-spaces.pipe';
+import { StarComponent } from '../shared/star.component';
+import { RouterModule } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { ProductDetailsGuard } from './product-details.guard';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    ProductListComponent,
+    ProductDetailsComponent,
+    ConvertToSpacesPipe,
+    StarComponent
+  ],
   imports: [
-    CommonModule
+    CommonModule,
+    FormsModule,
+    RouterModule.forChild([
+      { path: 'products', component: ProductListComponent },
+      { path: 'products/:id',
+        canActivate: [ProductDetailsGuard],
+        component: ProductDetailsComponent
+       }
+    ])
+   
   ]
 })
 export class ProductModule { }
