bc31fc3640230df6c53addc00df10526628d54d8
kasia.kordys <kasia.kordys@gmail.com>
2021-12-02 17:56:07 +0200

    move shared items to shared module

--- APM/src/app/products/product.module.ts
+++ APM/src/app/products/product.module.ts
@@ -1,11 +1,8 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
 import { ProductListComponent } from './product-list.component';
 import { ProductDetailsComponent } from './product-details.component';
 import { ConvertToSpacesPipe } from '../shared/convert-to-spaces.pipe';
-import { StarComponent } from '../shared/star.component';
 import { RouterModule } from '@angular/router';
-import { FormsModule } from '@angular/forms';
 import { ProductDetailsGuard } from './product-details.guard';
 import { SharedModule } from '../shared/shared.module';
 
@@ -14,11 +11,8 @@ import { SharedModule } from '../shared/shared.module';
     ProductListComponent,
     ProductDetailsComponent,
     ConvertToSpacesPipe,
-    StarComponent
   ],
   imports: [
-    CommonModule,
-    FormsModule,
     RouterModule.forChild([
       { path: 'products', component: ProductListComponent },
       { path: 'products/:id',
